163f9c1a7b69d3d875d654e0b4ba55490ce4040a
Slav <itiksolution@gmail.com>
2022-10-10 15:24:49 +1100

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+
+
+
+function findMatching(array, name) {
+
+  let match = array.filter((element) => {
+    if (element.toLowerCase() === name.toLowerCase()) return element
+  })
+return match
+}
+console.log(findMatching(drivers, 'Bobby'))
+
+
+
+function fuzzyMatch(array, name) {
+  let match = array.filter((element) => {
+    if (name === element.substring(0, 2)) return element
+  })
+
+  return match
+}
+console.log(fuzzyMatch(drivers, 'Sa'))
+
+
+
+function matchName(array, name) {
+  let match = array.filter((element) => {
+    if (name === element.name) {
+      return element
+    }
+  })
+  return match
+}
+
+console.log(matchName(drivers, 'Bobby'));
